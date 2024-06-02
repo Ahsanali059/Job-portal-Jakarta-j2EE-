5d788efee2376e6cd89fef5a60afefbad20f560b
Ahsanali059 <AhsanDev.org@gmail.com>
2024-06-02 16:18:09 +0500

    added job portal webssite

---
+++ uberProject-ReviewService/src/test/java/com/example/uberprojectreviewservice/UberProjectReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uberprojectreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
